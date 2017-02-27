9fd91ef4497100645259549dfbc4c9481e4db2de
Crispioso <crispin.merriman@gmail.com>
2017-02-27 21:21:03 +0000

    Stop page refresh on workspace click

--- views/components/sidebar/Workspaces.jsx
+++ views/components/sidebar/Workspaces.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import MenuItem from 'material-ui/MenuItem';
 
 export default class Workspaces extends Component {
     render() {
         return (
-            <ul style={{padding:0}}>
+            <div style={{padding:0}}>
                 {this.props.workspaces.map(workspace => {
-                    return <MenuItem href={workspace.id} key={workspace.id}>{workspace.id}</MenuItem>
+                    return <MenuItem key={workspace.id} containerElement={<Link to={workspace.id} />}>{workspace.id}</MenuItem>;
                 })}
-            </ul>
+            </div>
         )
     }
 }
